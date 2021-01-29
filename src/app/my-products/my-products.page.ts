a86ef51aca3d44559ccc699f9106f27048e2ffb0
juan_lujan <juanilloxyz@hotmail.com>
2021-01-29 20:45:57 +0100

    elimino articulos

--- src/app/my-products/my-products.page.ts
+++ src/app/my-products/my-products.page.ts
@@ -1,70 +1,118 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Iarticulo, Iinmobiliaria, Imotor, Itecnologia} from '../interfaces';
 import {ArticuloService} from '../services/articulo.service';
+import {AngularFireDatabase} from '@angular/fire/database';
+import {ToastController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-my-products',
-  templateUrl: './my-products.page.html',
-  styleUrls: ['./my-products.page.scss'],
+    selector: 'app-my-products',
+    templateUrl: './my-products.page.html',
+    styleUrls: ['./my-products.page.scss'],
 })
 export class MyProductsPage implements OnInit {
 
-  motor: (Iarticulo | Imotor)[] = [];
-  inmobiliaria: (Iarticulo | Iinmobiliaria)[] = [];
-  tecnologia: (Iarticulo | Itecnologia)[] = [];
+    motor: (Iarticulo | Imotor)[] = [];
+    inmobiliaria: (Iarticulo | Iinmobiliaria)[] = [];
+    tecnologia: (Iarticulo | Itecnologia)[] = [];
 
-  oculto1: boolean = false;
-  oculto2: boolean = false;
-  oculto3: boolean = false;
+    oculto1: boolean = false;
+    oculto2: boolean = false;
+    oculto3: boolean = false;
 
-  cambiar_oculto1() {
-    this.oculto1 = true;
-    this.oculto2 = false;
-    this.oculto3 = false;
-  }
+    cambiar_oculto1() {
+        this.oculto1 = true;
+        this.oculto2 = false;
+        this.oculto3 = false;
+    }
 
-  cambiar_oculto2() {
-    this.oculto1 = false;
-    this.oculto2 = true;
-    this.oculto3 = false;
-  }
+    cambiar_oculto2() {
+        this.oculto1 = false;
+        this.oculto2 = true;
+        this.oculto3 = false;
+    }
 
-  cambiar_oculto3() {
-    this.oculto1 = false;
-    this.oculto2 = false;
-    this.oculto3 = true;
-  }
+    cambiar_oculto3() {
+        this.oculto1 = false;
+        this.oculto2 = false;
+        this.oculto3 = true;
+    }
 
-  cambiar_oculto4() {
-    this.oculto1 = false;
-    this.oculto2 = false;
-    this.oculto3 = false;
-  }
+    cambiar_oculto4() {
+        this.oculto1 = false;
+        this.oculto2 = false;
+        this.oculto3 = false;
+    }
 
-  constructor(private _articuloService: ArticuloService) { }
+    constructor(private _articuloService: ArticuloService, private _db: AngularFireDatabase, private _toastCtrl: ToastController) {
+    }
 
-  ngOnInit() {
-    this._articuloService.getMotor().once('value', snapshot => {
-      snapshot.forEach(child => {
-        let value = child.val();
-        value.key = child.key;
-        this.motor.push(value);
-      });
-    });
-    this._articuloService.getInmobiliaria().once('value', snapshot => {
-      snapshot.forEach(child => {
-        let value = child.val();
-        value.key = child.key;
-        this.inmobiliaria.push(value);
-      });
-    });
-    this._articuloService.getTecnologia().once('value', snapshot => {
-      snapshot.forEach(child => {
-        let value = child.val();
-        value.key = child.key;
-        this.tecnologia.push(value);
-      });
-    });
-  }
+    async presentToast() {
+        const toast = await this._toastCtrl.create({
+            message: 'Se ha eliminadp el articulo correctamente.',
+            duration: 2000,
+            position: 'bottom'
+        });
+        toast.present();
+    }
 
+    ngOnInit() {
+        this._articuloService.getMotor().once('value', snapshot => {
+            snapshot.forEach(child => {
+                let value = child.val();
+                value.key = child.key;
+                this.motor.push(value);
+            });
+        });
+        this._articuloService.getInmobiliaria().once('value', snapshot => {
+            snapshot.forEach(child => {
+                let value = child.val();
+                value.key = child.key;
+                this.inmobiliaria.push(value);
+            });
+        });
+        this._articuloService.getTecnologia().once('value', snapshot => {
+            snapshot.forEach(child => {
+                let value = child.val();
+                value.key = child.key;
+                this.tecnologia.push(value);
+            });
+        });
+    }
+
+    deleteMotor(nombre: string) {
+        let ref = this._db.database.ref('articulos/motor');
+        ref.orderByChild('nombre').equalTo(nombre).once('value', snapshot => {
+            this.motor = [];
+            snapshot.forEach(child => {
+                let clave = child.key;
+                ref.child(clave).remove();
+            });
+        });
+        this.presentToast();
+
+    }
+
+    deleteInmobiliara(nombre: string) {
+        let ref = this._db.database.ref('articulos/inmobiliaria');
+        ref.orderByChild('nombre').equalTo(nombre).once('value', snapshot => {
+            this.motor = [];
+            snapshot.forEach(child => {
+                let clave = child.key;
+                ref.child(clave).remove();
+            });
+        });
+        this.presentToast();
+    }
+
+    deleteTecnologia(nombre: string) {
+        let ref = this._db.database.ref('articulos/tecnologia');
+        ref.orderByChild('nombre').equalTo(nombre).once('value', snapshot => {
+            this.motor = [];
+            snapshot.forEach(child => {
+                let clave = child.key;
+                ref.child(clave).remove();
+            });
+        });
+        this.presentToast();
+    }
 }
