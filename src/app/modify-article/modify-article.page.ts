ee794f8dffc1430cb0fbd7976c29eb327c461d0c
juan_lujan <juanilloxyz@hotmail.com>
2021-01-30 10:47:40 +0100

    Termino de editar articulos

--- src/app/modify-article/modify-article.page.ts
+++ src/app/modify-article/modify-article.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Iarticulo, Iinmobiliaria, Imotor, Itecnologia} from '../interfaces';
 import {AngularFireDatabase} from '@angular/fire/database';
+import {ToastController} from '@ionic/angular';
 
 @Component({
     selector: 'app-modify-article',
@@ -9,27 +10,106 @@ import {AngularFireDatabase} from '@angular/fire/database';
     styleUrls: ['./modify-article.page.scss'],
 })
 export class ModifyArticlePage implements OnInit {
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    stateMotor: string;
+    kilometros: number;
+    anyos: number;
+    numeroHab: number;
+    localidad: string;
+    stateEstado: string;
+    property: string;
     name: string;
+    clave: string;
+
     articulos: (Iarticulo | Imotor | Iinmobiliaria | Itecnologia)[] = [];
 
-    constructor(private _activateRoute: ActivatedRoute, private _db: AngularFireDatabase) {
+    constructor(private _activateRoute: ActivatedRoute, private _db: AngularFireDatabase, private _toastCtrl: ToastController) {
+    }
+
+    async presentToast() {
+        const toast = await this._toastCtrl.create({
+            message: 'Se ha editapo el articulo correctamente.',
+            duration: 2000,
+            position: 'bottom'
+        });
+        toast.present();
     }
 
     ngOnInit() {
         this.name = this._activateRoute.snapshot.paramMap.get('name');
-        this.chargeArtile();
+        this.chargeArtileMotor();
+        this.chargeArticleInmobiliaria();
+        this.chargeArticleTecnologia();
     }
 
-    private chargeArtile() {
+    private chargeArtileMotor() {
 
         let ref = this._db.database.ref('articulos/motor');
         ref.orderByChild('nombre').equalTo(this.name).once('value', snapshot => {
             this.articulos = [];
             snapshot.forEach(child => {
-                this.articulos = child.val();
-                //quiero cargar en articulos todo el valor q recibre de chil.val() y en el html lo
-                //quiero mostrar para poder modificar con un boton que genere una funcion para ello
-            })
+                this.articulos.push(child.val());
+                this.clave = child.key;
+            });
         });
+    }
+
+    private chargeArticleInmobiliaria() {
+        let ref = this._db.database.ref('articulos/inmobiliaria');
+        ref.orderByChild('nombre').equalTo(this.name).once('value', snapshot => {
+            this.articulos = [];
+            snapshot.forEach(child => {
+                this.clave = child.key;
+            });
+        });
+    }
+
+    private chargeArticleTecnologia() {
+        let ref = this._db.database.ref('articulos/tecnologia');
+        ref.orderByChild('nombre').equalTo(this.name).once('value', snapshot => {
+            this.articulos = [];
+            snapshot.forEach(child => {
+                this.clave = child.key;
+            });
+        });
+    }
+
+    editMotor() {
+        let ref = this._db.database.ref('articulos/motor');
+        ref.child(this.clave).set({
+            nombre: this.nombre,
+            descripcion: this.descripcion,
+            precio: this.precio,
+            vehiculo: this.stateMotor,
+            kilometros: this.kilometros,
+            anyos: this.anyos
+        });
+        this.presentToast();
+    }
+
+
+    editInmobiliaria() {
+        let ref = this._db.database.ref('articulos/inmobiliaria');
+        ref.child(this.clave).set({
+            nombre: this.nombre,
+            descripcion: this.descripcion,
+            precio: this.precio,
+            numeroHabitaciones: this.numeroHab,
+            localidad: this.localidad,
+        });
+        this.presentToast();
+    }
+
+    editTecnologia() {
+        let ref = this._db.database.ref('articulos/tecnologia');
+        ref.child(this.clave).set({
+            nombre: this.nombre,
+            descripcion: this.descripcion,
+            precio: this.precio,
+            estado: this.stateEstado
+        });
+        this.presentToast();
     }
 }
